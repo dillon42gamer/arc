d25b8d2925eab330c1027b2ee80c13e424540934
dillon42gamer <160809922+dillon42gamer@users.noreply.github.com>
2024-06-03 14:50:49 -0700

    Add files via upload

---
+++ js/mathhelp/pre.js
@@ -0,0 +1,42 @@
+async function gotowithframe(site) {
+  try {
+    await registerSW();
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  frame.src = __uv$config.prefix + __uv$config.encodeUrl(site);
+};
+async function goto(site) {
+    try {
+        await registerSW();
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    
+      location.href = __uv$config.prefix + __uv$config.encodeUrl(site);
+}
+async function fullscreen() {
+  frame.requestFullscreen();
+}
+async function reload() {
+  frame.contentWindow.location.reload();
+}
+function transitionToPage(page) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function() { 
+      frame.src = location.href + page;
+  }, 500)
+}
+function transitionToPageWProxy(page) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function() { 
+      window.location.href = __uv$config.prefix + __uv$config.encodeUrl(page);
+  }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.querySelector('body').style.opacity = 1
+})
